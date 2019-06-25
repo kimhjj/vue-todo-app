2611b2719206bd3472d46ae14230a81cb7f8db71
kimhj <>
2019-06-25 20:14:34 +0900

    save local storage

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
+  <div class="inputBox shadow">
     <!-- 아래 input에 값을 입력하면, newTodoItem 변수에 값이 들어간다. -->
     <input type="text" v-model="newTodoItem">
-    <button>추가</button>
+    <button @click="addTodo">추가</button>
   </div>
 </template>
 
@@ -13,9 +13,48 @@ export default {
       newTodoItem: ''
     }
   },
+  methods: {
+    addTodo() {
+      // dom이 아니라, 변수의 값이 있는지 확인 (양방향, model)
+      if(this.newTodoItem !== '') {
+        // local storage에 data 추가
+        // 로직) addTodo 추가 버튼을 누르면 newTodoItem에 입력된 값을 local storage에 추가
+        // 확인) 개발자 도구의 application 탭에서 local storage 확인 가능
+        // 설명) setItem ==> key, value
+        localStorage.setItem(this.newTodoItem+'_key', this.newTodoItem);
+        this.clearInput();
+      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  },
 }
 </script>
 
-<style>
-
+<style scope>
+input:focus {
+outline: none;
+}
+.inputBox {
+background: white;
+height: 50px;
+line-height: 50px;
+border-radius: 5px;
+}
+.inputBox input {
+border-style: none;
+font-size: 0.9rem;
+}
+.addContainer {
+float: right;
+background: linear-gradient(to right, #6478FB, #8763FB);
+display: block;
+width: 3rem;
+border-radius: 0 5px 5px 0;
+}
+.addBtn {
+color: white;
+vertical-align: middle;
+}
 </style>
