9848e2ff3b4525093ba614baf4c1750a816e4957
kimhj <>
2019-06-25 19:16:43 +0900

    v-bind: ==> : , v-on: ==> @

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <!-- message-value 속성에 title 값을 담아서 자식에게 전달한다. -->
-    <HelloWorld v-bind:message-value="title" v-on:show-log="printText"/>
+    <HelloWorld :message-value="title" @show-log="printText"/>
   </div>
 </template>
 
