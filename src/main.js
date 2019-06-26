13965dba130aae50f0deee3f5eabe85cc00aa145
kimhj <>
2019-06-26 20:52:28 +0900

    vuex의 store 개념(컴포넌트 간 공유되는 데이터)을 사용하기위해 추가함

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// export default가 아니니까, { }를 붙인다.
+import { store } from './store/store';
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store,  // store는 루트의 모든 하위 컴포넌트에 주입된다. (App.vue에서 쓸거니까 injection)
 }).$mount('#app')
---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+// 루트 컴포넌트에 stroe 옵션을 제공
+// store는 루트의 모든 하위 컴포넌트에 주입된다.
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  //
+});
