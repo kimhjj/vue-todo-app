edac50767e6e9d0e3aa9c783d516bbf0f2a96c69
kimhj <>
2019-06-27 20:50:16 +0900

    repository 'vue-todo-server' ===> axios/VueAxios 임포트, 서버 추가

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 // 루트 컴포넌트에 stroe 옵션을 제공
 // store는 루트의 모든 하위 컴포넌트에 주입된다.
+// axios와 VueAxios를 임포트
 Vue.use(Vuex);
+Vue.use(VueAxios, axios);
 
 const storage = {
   fetch() {
